34038c8af04e5d0cf7b7f7efc0936e50cfe94707
deadlyaim <wnoesimaldonad06@gmail.com>
2026-07-16 22:01:14 -0400

    Update reviews.ts

--- src/data/reviews.ts
+++ src/data/reviews.ts
@@ -57,5 +57,19 @@ export const REVIEWS: Review[] = [
     rating: 5,
     comment: 'Llegó más rápido de lo esperado',
     photo: 'https://p16-oec-general-useast5.ttcdn-us.com/tos-useast5-i-omjb5zjo8w-tx/8bbca8af1fd7491e9fc1ec99aa6cdf87~tplv-fhlh96nyum-crop-webp:300:300.webp?dr=12190&t=555f072d&ps=933b5bde&shp=8dbd94bf&shcp=607f11de&idc=useast8&from=2378011839'
+  },
+  {
+    productId: 'prod-5',
+    productName: 'Stars Pattern Casual Set',
+    author: 's**b',
+    rating: 5,
+    comment: 'La ropa no está nada mal',
+    photo: 'https://p16-oec-general-useast5.ttcdn-us.com/tos-useast5-i-omjb5zjo8w-tx/81c6827a68994fd1826379a7b863d45e~tplv-fhlh96nyum-crop-webp:300:300.webp?dr=12190&t=555f072d&ps=933b5bde&shp=8dbd94bf&shcp=607f11de&idc=useast8&from=2378011839',
+    photos: [
+      'https://p16-oec-general-useast5.ttcdn-us.com/tos-useast5-i-omjb5zjo8w-tx/81c6827a68994fd1826379a7b863d45e~tplv-fhlh96nyum-crop-webp:300:300.webp?dr=12190&t=555f072d&ps=933b5bde&shp=8dbd94bf&shcp=607f11de&idc=useast8&from=2378011839',
+      'https://p16-oec-general-useast5.ttcdn-us.com/tos-useast5-i-omjb5zjo8w-tx/32fb6b3535194e6c999dd03005c348cc~tplv-fhlh96nyum-crop-webp:300:300.webp?dr=12190&t=555f072d&ps=933b5bde&shp=8dbd94bf&shcp=607f11de&idc=useast8&from=2378011839',
+      'https://p16-oec-general-useast5.ttcdn-us.com/tos-useast5-i-omjb5zjo8w-tx/9bb33624be56426e98dc57dbdd1d50ad~tplv-fhlh96nyum-crop-webp:300:300.webp?dr=12190&t=555f072d&ps=933b5bde&shp=8dbd94bf&shcp=607f11de&idc=useast8&from=2378011839',
+      'https://p16-oec-general-useast5.ttcdn-us.com/tos-useast5-i-omjb5zjo8w-tx/0e34358025d14fc2bf9e96d26989c7f4~tplv-fhlh96nyum-crop-webp:300:300.webp?dr=12190&t=555f072d&ps=933b5bde&shp=8dbd94bf&shcp=607f11de&idc=useast8&from=2378011839'
+    ]
   }
 ];
